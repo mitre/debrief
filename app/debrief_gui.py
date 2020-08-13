fe864e87a43e4c017aa3afb644863fac9d0863a3
ddavila54 <30359513+ddavila54@users.noreply.github.com>
2020-08-13 05:02:37 -0400

    Update debrief_gui.py

--- app/debrief_gui.py
+++ app/debrief_gui.py
@@ -1,13 +1,54 @@
+import base64
 import json
 import logging
+from io import BytesIO
 
 from aiohttp import web
 from aiohttp_jinja2 import template
-from reportlab.pdfgen import canvas
+from reportlab.graphics import renderPM
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.units import inch
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, PageBreak, Image, Flowable
+from svglib.svglib import svg2rlg
 
 from app.service.auth_svc import for_all_public_methods, check_authorization
 from app.utility.base_world import BaseWorld
 from plugins.debrief.app.debrief_svc import DebriefService
+from plugins.debrief.app.crop import Crop
+
+
+class DrawLine(Flowable):
+    def __init__(self):
+        Flowable.__init__(self)
+
+    def draw(self):
+        maroon = colors.maroon
+        self.canv.setStrokeColor(maroon)
+        self.canv.setLineWidth(4)
+        self.canv.line(110, 30, 450, 30)
+
+
+def _status_name(status):
+    if status == 0:
+        return 'success'
+    elif status == -2:
+        return 'discarded'
+    elif status == 1:
+        return 'failure'
+    elif status == 124:
+        return 'timeout'
+    elif status == -3:
+        return 'collected'
+    elif status == -4:
+        return 'untrusted'
+    elif status == -5:
+        return 'visibility'
+    else:
+        return 'queued'
 
 
 @for_all_public_methods(check_authorization)
@@ -30,7 +71,8 @@ class DebriefGui(BaseWorld):
 
     @template('debrief.html')
     async def splash(self, request):
-        operations = [o.display for o in await self.data_svc.locate('operations', match=await self._get_access(request))]
+        operations = [o.display for o in
+                      await self.data_svc.locate('operations', match=await self._get_access(request))]
         return dict(operations=operations)
 
     async def report(self, request):
@@ -49,11 +91,16 @@ class DebriefGui(BaseWorld):
 
     async def download_pdf(self, request):
         data = dict(await request.json())
+        svg_bytes = data['graph']
+        with open('./graph.svg', "wb") as fh:
+            fh.write(base64.b64decode(svg_bytes))
+        png_convert = svg2rlg("./graph.svg")
+        renderPM.drawToFile(png_convert, "./graph.png", fmt="PNG")
         if data['operation_id'] and len(data['operation_id']) == 1:
             op_id = data['operation_id'].pop()
             operation = (await self.data_svc.locate('operations', match=dict(id=int(op_id))))[0]
             filename = operation.name + '_' + operation.start.strftime('%Y-%m-%d_%H-%M-%S')
-            pdf_bytes = self._build_pdf(filename, operation)
+            pdf_bytes = self._build_pdf(operation)
             return web.json_response(dict(filename=filename, pdf_bytes=pdf_bytes))
         return web.json_response('None or multiple operations selected')
 
@@ -65,31 +112,121 @@ class DebriefGui(BaseWorld):
         except Exception as e:
             self.log.error(repr(e), exc_info=True)
 
-    def _build_pdf(self, filename, operation):
-        c = canvas.Canvas(filename + '.pdf', bottomup=0)
-        c.setTitle(filename)
-        self._draw_logo(c)
-        c.setFont('Helvetica-Bold', 20)
-        c.drawString(100, 100, operation.name)
-        c.setFont('Helvetica', 15)
-        c.drawString(100, 140, 'Steps')
-        self._draw_steps(c, operation.chain)
-        c.showPage()
-        return c.getpdfdata().decode('utf-8', errors='ignore')
-
     @staticmethod
-    def _draw_logo(cnvs):
-        cnvs.saveState()
-        cnvs.translate(15, 15)
-        cnvs.scale(1, -1)
-        cnvs.drawImage('./static/img/back-red.png', 80, 0, -80, -60)
-        cnvs.restoreState()
+    def _build_pdf(operation):
+        pdf_buffer = BytesIO()
+        doc = SimpleDocTemplate(pdf_buffer, pagesize=letter,
+                                rightMargin=72, leftMargin=72,
+                                topMargin=18, bottomMargin=18)
+        Story = []
+        styles = getSampleStyleSheet()
+        title = styles['Heading1']
+        normal = styles['Normal']
+        title.alignment = 1
+        logo = "./static/img/back-red.png"
+        im = Image(logo, 1.5 * inch, 1 * inch)
+        im.hAlign = 'LEFT'
+        Story.append(im)
+        line = DrawLine()
+        Story.append(line)
+        Story.append(Spacer(1, 12))
+        op_name = operation.name
+        pdfmetrics.registerFont(TTFont('VeraBd', 'VeraBd.ttf'))
+        text = "<font name =VeraBd size=16 color=maroon>OPERATION DEBRIEF</font>"
+        Story.append(Paragraph(text, title))
+        Story.append(Spacer(1, 12))
+        text = "<u><font name =Helvetica-Bold size=12>OPERATION</font></u><font size=12> = %s</font>" % op_name
+        Story.append(Paragraph(text, normal))
+        Story.append(Spacer(1, 12))
+        Story.append(Spacer(1, 12))
+        Story.append(Spacer(1, 12))
+        ptext = '<font size="12">GRAPH</font>'
+        Story.append(Paragraph(ptext, title))
+        Crop.crop_image('./graph.png', (450, 110, 800, 300), './graph_cropped.png')
+        graph = "./graph_cropped.png"
+        gr = Image(graph, 5 * inch, 2.5 * inch)
+        Story.append(gr)
+        Story.append(Spacer(1, 12))
+        Story.append(Spacer(1, 12))
+        ptext = '<font size="12">OPERATION STATS</font>'
+        Story.append(Paragraph(ptext, title))
+        time = operation.start.strftime('%Y-%m-%d %H:%M:%S')
+        data = [['State', 'Planner', 'Objective', 'Time'],
+                [operation.state, operation.planner.name, operation.objective.name, time]]
+        t = Table(data)
+        t.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.maroon),
+                               ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+                               ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
+                               ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                               ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+                               ('INNERGRID', (0, 0), (-1, -1), 0.5, colors.black),
+                               ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
+                               ]))
+        Story.append(t)
+        Story.append(PageBreak())
+        Story.append(im)
+        line = DrawLine()
+        Story.append(line)
+        Story.append(Spacer(1, 12))
+        Story.append(Spacer(1, 12))
+        ptext = '<font size="12">OPERATION STEPS</font>'
+        Story.append(Paragraph(ptext, title))
+        count = 1
+        step_num = 1
+        for link in operation.chain:
+            if count == 6:
+                Story.append(PageBreak())
+                Story.append(im)
+                line = DrawLine()
+                Story.append(line)
+                Story.append(Spacer(1, 12))
+                Story.append(Spacer(1, 12))
+                count = 1
+            ptext = '<font size="12" name = Courier-Bold>%s.</font>' % step_num
+            Story.append(Paragraph(ptext, normal))
+            link_time = link.collect.strftime('%Y-%m-%d %H:%M:%S')
+            steps = [['Status', 'Time', 'Name', 'Agent'],
+                     [_status_name(link.status), link_time, link.ability.name, link.paw]]
+            s = Table(steps)
+            s.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.maroon),
+                                   ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+                                   ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
+                                   ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                                   ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+                                   ('FONTSIZE', (0, 0), (-1, -1), 10),
+                                   ('INNERGRID', (0, 0), (-1, -1), 0.5, colors.black),
+                                   ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
+                                   ]))
+            s.hAlign = 'CENTER'
+            Story.append(s)
 
-    @staticmethod
-    def _draw_steps(cnvs, chain):
-        cnvs.setFont('Helvetica', 12)
-        y = 140
-        for link in chain:
-            y += 20
-            cnvs.drawString(100, y, link.collect.strftime('%Y-%m-%d %H:%M:%S') + ' ' + link.ability.name)
+            # Create list of gathered facts, needs to be properly formatted in table
+            '''
+            facts_gathered = []
+            for f in link.facts:
+                if f.score > 0:
+                    facts_gathered.append(f.value)
+            '''
+            steps_2 = [['Command', 'Facts'],
+                       [base64.b64decode(link.command), '2 + 2 = 4']]
+            s2 = Table(steps_2)
+            s2.setStyle(TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.maroon),
+                                    ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
+                                    ('FONTNAME', (0, 0), (-1, 0), 'Courier-Bold'),
+                                    ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                                    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+                                    ('FONTSIZE', (0, 0), (-1, -1), 10),
+                                    ('INNERGRID', (0, 0), (-1, -1), 0.5, colors.black),
+                                    ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
+                                    ]))
+            s2.hAlign = 'CENTER'
+            Story.append(s2)
+            Story.append(Spacer(1, 12))
+            step_num += 1
+            count += 1
+        doc.build(Story)
+        pdf_value = pdf_buffer.getvalue()
+        pdf_buffer.close()
+        return pdf_value.decode('utf-8', errors='ignore')
+
 
